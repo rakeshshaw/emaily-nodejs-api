e2648527432681ad5ff7ef198b790b75b2fbb82d
Rakesh Shaw11 <rashaw11@in.ibm.com>
2022-09-21 19:03:20 +0530

    gitignore testing

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js - production keys here
+module.exports = {
+  googleClientID: process.env.DEV_GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.DEV_GOOGLE_CLIENT_SECRET,
+  cookieKey: process.env.DEV_COOKIE_KEY,
+  mongoURI: process.env.DEV_MONGO_URI,
+};
