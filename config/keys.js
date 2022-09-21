484779c78658655d93d48dfc26c28b20e5d44279
Rakesh Shaw11 <rashaw11@in.ibm.com>
2022-09-21 18:50:09 +0530

    gitignore testing

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
-module.exports = {
-  googleClientID: '882035264568-9mb05k3ieac6vhh78ub8ufckfqsb9547.apps.googleusercontent.com',
-  googleClientSecret: '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',
-  cookieKey: 'axcsvdffdgfdgfdgf',
-  mongoURI: 'mongodb://localhost:27017/emaily'
-};
+// keys.js - figure out what set of credentials to return
+if(process.env.NODE_ENV === 'production') {
+  // we are in production - return prod set of keys
+  module.exports = require('./prod');
+
+} else {
+  // we are in development - return dev keys
+  module.exports = require('./dev');
+}
