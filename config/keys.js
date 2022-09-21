893dd60d84dd355f1bfb842c5cded3956095b1f9
rakeshshaw <rakeshshaw@gmail.com>
2022-09-21 18:40:38 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '882035264568-9mb05k3ieac6vhh78ub8ufckfqsb9547.apps.googleusercontent.com',
-  googleClientSecret: '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',
-  cookieKey: 'axcsvdffdgfdgfdgf',
-  mongoURI: 'mongodb://localhost:27017/emaily'
-};
